51de292aaad38da45d4c46b3feebb13f6884dabd
Ethan-Nowa <140451419+Ethan-Nowa@users.noreply.github.com>
2024-01-24 21:33:35 -0500

    Update Rental.java

--- src/mine/java/bad/robot/refactoring/chapter1/Rental.java
+++ src/mine/java/bad/robot/refactoring/chapter1/Rental.java
@@ -1,4 +1,3 @@
-package bad.robot.refactoring.chapter1;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,5 +19,31 @@ public class Rental {
     public int getDaysRented() {
         return daysRented;
     }
+
+	double getChargeFor() {
+		double amount = 0;
+		switch (getMovie().getPriceCode()) {
+		    case Movie.REGULAR:
+		        amount += 2;
+		        if (getDaysRented() > 2)
+		            amount += (getDaysRented() - 2) * 1.5;
+		        break;
+		    case Movie.NEW_RELEASE:
+		        amount += getDaysRented() * 3;
+		        break;
+		    case Movie.CHILDREN:
+		        amount += 1.5;
+		        if (getDaysRented() > 3)
+		            amount += (getDaysRented() - 3) * 1.5;
+		        break;
+		}
+		return amount;
+	}
+
+	int calculateFrequentRenterPoints(int frequentRenterPoints) {
+		if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1)
+		    return 2;
+		return 1;
+	}
 
 }
